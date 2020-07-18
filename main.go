e31e7c6da04974323029bee96e4e2071bfd5b1f9
Andrey Hernandez <andrey_hz@outlook.com>
2020-07-17 23:00:47 -0500

    Principal

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/AndreyAlonso/BasesDeDatos-Go/storage"
+
+func main() {
+	storage.NewPostgresDB()
+}
---
+++ storage/storage.go
@@ -0,0 +1,37 @@
+package storage
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"sync"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	db   *sql.DB
+	once sync.Once
+)
+
+func NewPostgresDB() {
+	once.Do(func() {
+		var err error
+		db, err = sql.Open("postgres", "user=postgres dbname=EDteam-dev sslmode=disable")
+		if err != nil {
+			log.Fatalf("No se puede abrir la base de datos: %v", err)
+		}
+
+		if err = db.Ping(); err != nil {
+			log.Fatalf("No se puede hacer ping con la base de datos: %v", err)
+		}
+
+	})
+	fmt.Println("Conectado a Postgres")
+
+}
+
+// Pool retorna una unica instancia de db
+func Pool() *sql.DB {
+	return db
+}
